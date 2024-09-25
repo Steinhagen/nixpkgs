396d1caadf9851c43d98c4cc35f8319dd8fcb36b
Sebastian Sellmeier <mail@sebastian-sellmeier.de>
2024-09-25 17:47:35 +0200

    proton-vpn-local-agent: init at unstable-20241010

--- pkgs/applications/networking/protonvpn-gui/default.nix
+++ pkgs/applications/networking/protonvpn-gui/default.nix
@@ -14,6 +14,7 @@
   proton-vpn-connection,
   proton-vpn-killswitch,
   proton-vpn-killswitch-network-manager,
+  proton-vpn-local-agent,
   proton-vpn-logger,
   proton-vpn-network-manager,
   proton-vpn-network-manager-openvpn,
@@ -65,6 +66,7 @@ buildPythonApplication rec {
     proton-vpn-connection
     proton-vpn-killswitch
     proton-vpn-killswitch-network-manager
+    proton-vpn-local-agent
     proton-vpn-logger
     proton-vpn-network-manager
     proton-vpn-network-manager-openvpn
